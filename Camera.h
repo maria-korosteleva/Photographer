4b9a05c445b52da4b0f2d7e36ddf999a441e1efa
Maria Korosteleva <mariako@kaist.ac.kr>
2019-06-17 15:01:04 +0900

    restructure the folder and add static library builder

---
+++ Camera.h
@@ -0,0 +1,91 @@
+#pragma once
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+// It also holds the projection matrix
+// Move rotations to something better
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Camera
+{
+public:
+    enum Directions {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    };
+    enum Mode
+    {
+        TARGET_MODE, 
+        FREE_MODE
+    };
+
+    Camera(int screen_width, int screen_height, float field_of_view = default_fov_);
+    ~Camera();
+
+    unsigned int getID() { return ID_; }
+    glm::vec3 getPosition() { return position_; }
+    glm::vec3 getFrontVector() { return front_; }
+    
+    glm::mat4 getGlViewMatrix();
+    glm::mat4 getCVExtrinsicsMatrix();
+    glm::mat4 getGlProjectionMatrix();
+    glm::mat3 getCVIntrinsicsMatrix();
+
+    // allows to set pre-defined id
+    void setID(unsigned int id) { ID_ = id; };
+
+    void setPosition(glm::vec3 pos);
+    void setRotation(float pitch, float yaw);
+    void setTarget(glm::vec3 target);
+
+    void movePosition(Directions direction, float step_size_multiplier = 1.0f);
+    void updateRotation(float delta_pitch, float delta_yaw, bool constrain_pitch = true);
+    void zoom(float delta);
+
+    void saveParamsForOpenCV(const std::string path = "./");
+
+private:
+    static unsigned int avalible_camera_id;
+    static constexpr float default_fov_ = 35.0f;
+    // manipulation
+    static constexpr float rotation_sensitivity_ = 0.05f;
+    static constexpr float zoom_sensitivity_ = 0.1f;
+    static constexpr float movement_speed_ = 2.5f;
+
+    // Calculates the front vector 
+    // from the Camera's (updated) Euler Angles
+    void updateVectorsByRotation_();
+    // based on the target
+    void updateFrontByTarget_();
+
+    // ID
+    unsigned int ID_;
+
+    // mode
+    Mode mode_;
+
+    // extrinsic
+    glm::vec3 position_;
+    glm::vec3 front_;
+    glm::vec3 up_;
+    glm::vec3 right_;
+    const glm::vec3 world_up_ = glm::vec3(0.0f, 1.0f, 0.0f);
+    // extra -- rotation parameters
+    float yaw_, pitch_;
+    glm::vec3 target_;
+
+    // intrinsic 
+    float field_of_view_y_;
+    float screen_width_;
+    float screen_height_;
+
+};
+
---
+++ src/Shader.cpp
@@ -0,0 +1,196 @@
+#include "../Shader.h"
+
+
+Shader::Shader(ShaderTypes vertex_shader_type, ShaderTypes fragment_shader_type)
+{
+    unsigned int vertex_shader;
+    unsigned int fragment_shader;
+
+    // Compile requested shaders
+    switch (vertex_shader_type)
+    {
+    case ShaderTypes::FULL_SHADER:
+        vertex_shader = Shader::compileVertexShader_(full_vertex_shader_source);
+        break;
+    case ShaderTypes::SIMPLE_SHADER:
+        vertex_shader = Shader::compileVertexShader_(default_vertex_shader_source_);
+        break;
+    }
+
+    switch (fragment_shader_type)
+    {
+    case ShaderTypes::FULL_SHADER:
+        fragment_shader = Shader::compileFragmentShader_(full_fragment_shader_source);
+        break;
+    case ShaderTypes::SIMPLE_SHADER:
+        fragment_shader = Shader::compileFragmentShader_(default_fragment_shader_source_);
+        break;
+    }
+
+    // create program
+    createProgram_(vertex_shader, fragment_shader);
+
+    // cleanup
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+Shader::Shader(const GLchar * vertex_path, const GLchar * fragment_path)
+{
+    unsigned int vertex_shader;
+    unsigned int fragment_shader;
+    // read & compile
+    if (vertex_path != nullptr)
+    {
+        std::string vertex_code = Shader::readCodeFile_(vertex_path);
+        vertex_shader = Shader::compileVertexShader_(vertex_code.c_str());
+    }
+    else
+    {
+        vertex_shader = Shader::compileVertexShader_(default_vertex_shader_source_);
+    }
+    
+    if (fragment_path != nullptr)
+    {
+        std::string fragment_code = Shader::readCodeFile_(fragment_path);
+        fragment_shader = Shader::compileFragmentShader_(fragment_code.c_str());
+    }
+    else
+    {
+        fragment_shader = Shader::compileFragmentShader_(default_fragment_shader_source_);
+    }
+    
+    // create program
+    createProgram_(vertex_shader, fragment_shader);
+
+    // cleanup
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+Shader::~Shader()
+{
+    glDeleteProgram(ID_);
+}
+
+void Shader::use()
+{
+    glUseProgram(ID_);
+}
+
+void Shader::setUniform(const std::string & name, bool value) const
+{
+    glUniform1i(glGetUniformLocation(ID_, name.c_str()), (int)value);
+}
+
+void Shader::setUniform(const std::string & name, int value) const
+{
+    glUniform1i(glGetUniformLocation(ID_, name.c_str()), value);
+}
+
+void Shader::setUniform(const std::string & name, float value) const
+{
+    glUniform1f(glGetUniformLocation(ID_, name.c_str()), value);
+}
+
+void Shader::setUniform(const std::string & name, glm::mat4 value) const
+{
+    unsigned int location = glGetUniformLocation(ID_, name.c_str());
+    glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void Shader::setUniform(const std::string & name, glm::vec3 value) const
+{
+    unsigned int location = glGetUniformLocation(ID_, name.c_str());
+    glUniform3fv(location, 1, glm::value_ptr(value));
+}
+
+void Shader::setUniform(const std::string & name, glm::vec4 value) const
+{
+    unsigned int location = glGetUniformLocation(ID_, name.c_str());
+    glUniform4fv(location, 1, glm::value_ptr(value));
+}
+
+void Shader::createProgram_(unsigned int vertex_shader, unsigned int fragment_shader)
+{
+    // link shaders
+    ID_ = glCreateProgram();
+    glAttachShader(ID_, vertex_shader);
+    glAttachShader(ID_, fragment_shader);
+    glLinkProgram(ID_);
+
+    // check for linking errors
+    int success;
+    char infoLog[512];
+    glGetProgramiv(ID_, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(ID_, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+std::string Shader::readCodeFile_(const GLchar * path)
+{
+    std::string code;
+    std::ifstream file;
+
+    // allow throuwing exeptions
+    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try 
+    {
+        file.open(path);
+        std::stringstream stream;
+        stream << file.rdbuf();
+        file.close();
+        code = stream.str();
+    }
+    catch (std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE NOT SUCCESFULLY READ" << std::endl;
+    }
+    return code;
+}
+
+unsigned int Shader::compileVertexShader_(const char * shader_code)
+{
+    unsigned int vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertex_shader_id, 1, &shader_code, NULL);
+    glCompileShader(vertex_shader_id);
+
+    // check if compiled ok
+    int  success;
+    char info_log[512];
+    glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        // info before dying young 
+        glGetShaderInfoLog(vertex_shader_id, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+        return 0;
+    }
+
+    return vertex_shader_id;
+}
+
+unsigned int Shader::compileFragmentShader_(const char * shader_code)
+{
+    unsigned int fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragment_shader_id, 1, &shader_code, NULL);
+    glCompileShader(fragment_shader_id);
+
+    // check if compiled ok
+    int  success;
+    char info_log[512];
+    glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        // info before dying young 
+        glGetShaderInfoLog(fragment_shader_id, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << std::endl;
+        return 0;
+    }
+
+    return fragment_shader_id;
+}
